07deaff511d4fcc2162063b37f2bd35c7276c4aa
jasonjdominguez99 <jasonjdominguez99@users.noreply.github.com>
2022-12-03 14:10:57 +0000

    Completed python solution for part 1 of day 1

---
+++ day01/part1.py
@@ -0,0 +1,24 @@
+# part1
+
+# Python solution for part 1 of day 1 of 2022 'Advent of Code'
+#
+# This code outputs the highest calorie count for elves
+# from an input file where different elf calories are
+# separated an empty line
+
+def main():
+    max_calorie_count = 0
+    with open("input.txt") as file:
+        calorie_count = 0
+        for line in file:
+            if line == "\n":
+                max_calorie_count = max(max_calorie_count, calorie_count)
+                calorie_count = 0
+            else:
+                calorie_count += int(line.replace("\n", ""))
+    
+    print(max_calorie_count)
+
+if __name__ == "__main__":
+    main()
+    
